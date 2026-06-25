87d2ab748d3da0097acd9c3c7de091af3d702c95
Kavitha Kesavalu <kavithakesavalu@mac.mynetworksettings.com>
2026-06-25 17:04:57 -0400

    o

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,1 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/api(.*)',
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth.protect();
-  }
-});
+export default function middleware() {}
