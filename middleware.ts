c2bc1722c907e857a514fe88fb1e44c2d612c751
Kavitha Kesavalu <kavithakesavalu@mac.mynetworksettings.com>
2026-06-27 14:04:09 -0400

    PRINTING DEBUGGING

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
+console.log("CLERK_SECRET_KEY:", process.env.CLERK_SECRET_KEY);
 export default clerkMiddleware();
 
 export const config = {
